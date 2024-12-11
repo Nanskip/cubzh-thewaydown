1c1180f5a8c97f4e47ac0a71f51d17ee79737892
Nanskip <nanskip.oof@gmail.com>
2024-12-11 07:22:41 +0200

    test

--- modules/worldgen.lua
+++ modules/worldgen.lua
@@ -5,20 +5,50 @@ function mod.INIT()
 end
 
 mod.tiles = {
-    [0] = Color(79, 55, 35),
-    [1] = Color(107, 114, 115),
-    [2] = Color(69, 71, 71)
+    [0] = {
+        name = "dirt",
+        color = Color(79, 55, 35),
+        maxHeight = 10,
+        minHeight = 0,
+        offset = 2,
+        health = 1,
+    },
+    [1] = {
+        name = "stone",
+        color = Color(107, 114, 115),
+        maxHeight = 20,
+        minHeight = 10,
+        offset = 2,
+        health = 2,
+    },
+    [2] = {
+        name = "granite",
+        color = Color(69, 71, 71),
+        maxHeight = 30,
+        minHeight = 20,
+        offset = 2,
+        health = 4,
+    }
 }
 
 function mod.generate()
-    local height = 30
     local width = 10
 
     mod.map = {}
-    for y = 0, height do
-        mod.map[y] = {}
-        for x = 0, width do
-            mod.map[y][x] = math.random(0, 2)
+    for i=0, #mod.tiles do
+        local tile = mod.tiles[i]
+
+        for y = 0 + tile.minHeight, tile.minHeight + tile.maxHeight + tile.offset do
+            mod.map[y] = {}
+            for x = 0, width do
+                if y <= tile.minHeight + tile.maxHeight then
+                    if mod.map[y][x] ~= nil then
+                        mod.map[y][x] = i
+                    end
+                else
+                    mod.map[y][x] = i + math.random(0, 1)
+                end
+            end
         end
     end
 end
@@ -27,12 +57,13 @@ function mod.build()
     mod.mapObject = MutableShape()
     local height = 30
     local width = 10
-    
+
     for y = 0, height do
         for x = 0, width do
-            local color = mod.tiles[mod.map[y][x]]
-            if color ~= nil then
-                mod.mapObject:AddBlock(color, x, y, 0)
+            local tile = mod.tiles[mod.map[y][x]]
+
+            if tile ~= nil then
+                mod.mapObject:AddBlock(tile.color, Number3(x, y, 0))
             end
         end
     end
